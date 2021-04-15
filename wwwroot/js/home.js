39c57fc73d4c16e738485d570fc52fad9e82e6d4
beboantonio <beboantonio1@hotmail.com>
2021-04-15 00:07:01 -0300

    add email validation

--- wwwroot/js/home.js
+++ wwwroot/js/home.js
@@ -1,20 +1,51 @@
 
+
 (function () {
   
-    var forms = document.querySelectorAll('.needs-validation')
-    
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()                       
-          }
+  var forms = document.querySelectorAll('.needs-validation')
 
-          form.classList.add('was-validated')
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        event.preventDefault()
+
+        if (!form.checkValidity()) {
+          console.log("IN CHECK VALIDITY!!!")
+          event.stopPropagation()                       
+        }
+        else
+        {
+          const emailInput = form.elements.email;
+          const passwordInput = form.elements.password;
+    
+          console.log(emailInput.value, passwordInput.value );
+
+          if (!isValidEmail(emailInput.value))
+          {
+            console.log("INVALID EMAIL!!")
+
+            // Reset values
+            emailInput.value = ""
+            passwordInput.value = ""
+
+            return false;
+          }
           
-        }, false)
-      }
-    )
+
+          console.log("NOW CAN SEND FORM!!!")
+        }
+
+        form.classList.add('was-validated')
+        
+      }, false)
+    }
+  )
 })()
 
+
+function isValidEmail(email) 
+{
+  var emailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  return email.match(emailFormat)
+
+}
